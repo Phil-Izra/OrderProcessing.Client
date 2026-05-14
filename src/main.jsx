7b388cae47ca52a6d950c829478a6f6d264def22
Phil-Izra <philani.p@outlook.com>
2026-05-14 21:14:39 +0200

    change:updated code for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './auth/AuthContext';
+import App from './App';
+import theme from './theme/theme';
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<QueryClientProvider client={queryClient}>
+<ThemeProvider theme={theme}>
+<CssBaseline />
+<BrowserRouter>
+<AuthProvider>
+<App />
+</AuthProvider>
+</BrowserRouter>
+</ThemeProvider>
+</QueryClientProvider>
+</React.StrictMode>
+);
